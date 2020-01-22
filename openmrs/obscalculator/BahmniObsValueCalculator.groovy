3cc0e0fbfc2f447a5ed4d333ae574adafbbc68c4
sreekalyanthoughtworks <sreekalyan.bapatla@thirdware.com>
2020-01-22 17:15:57 +0530

    Sreekalyan | MOBN-796 | Changing the control id of the concepts height

--- openmrs/obscalculator/BahmniObsValueCalculator.groovy
+++ openmrs/obscalculator/BahmniObsValueCalculator.groovy
@@ -60,7 +60,7 @@ public class BahmniObsValueCalculator implements ObsValueCalculator {
     static final String OPD_OBS_DN4_SUM = "/76-0";
     static final String IME_OBS_DN4_SUM = "/61-0/136-0";
     static final String IPN_OBS_DN4_SUM = "/74-0";
-    static final String ANA_OBS_DN4_SUM = "/48-0";
+    static final String ANA_OBS_DN4_SUM = "/46-0";
 
     static List<String> paBalanceSectionControlIDs = Arrays.asList("/35-0", "/36-0", "/139-0", "/140-0", "/141-0", "/325-0", "/144-0", "/145-0", "/146-0", "/147-0");
     static List<String> paGaitSectionControlIDs = Arrays.asList("/38-0", "/39-0", "/149-0", "/150-0", "/151-0", "/152-0", "/153-0", "/154-0");
@@ -77,7 +77,7 @@ public class BahmniObsValueCalculator implements ObsValueCalculator {
     static List<String> opdDN4SectionControlIDs = Arrays.asList("/65-0", "/66-0", "/67-0", "/68-0", "/69-0", "/70-0", "/71-0","/72-0", "/73-0", "/74-0");
     static List<String> imeDN4SectionControlIDs = Arrays.asList("/61-0/62-0", "/61-0/63-0", "/61-0/64-0", "/61-0/65-0", "/61-0/67-0", "/61-0/68-0","/61-0/69-0", "/61-0/70-0", "/61-0/71-0", "/61-0/72-0");
     static List<String> ipnDN4SectionControlIDs = Arrays.asList("/26-0", "/27-0", "/28-0", "/30-0", "/31-0", "/33-0", "/34-0","/36-0", "/37-0", "/40-0");
-    static List<String> anaSectionControlIDs = Arrays.asList("/7-0", "/8-0");
+    static List<String> anaSectionControlIDs = Arrays.asList("/7-0", "/48-0");
     static String imePrefix = "/49-";
 
     def static finalScore = ["0.0", "8.5", "14.4", "18.6", "21.7", "24.3", "26.5", "28.4", "30.1", "31.7",
